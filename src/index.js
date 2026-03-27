6e9e787dbb0487ef5af24a4581fefdf6361892fe
Raynald Ramadhani Fachriansyah <raynaldfachriansyah@gmail.com>
2026-03-27 19:27:05 +0700

    testing CI/CD pipeline

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ const PORT = process.env.PORT || 8080;
 
 app.get("/health", (req, res) => {
   res.json({
-    nama: "Raynald Ramadhani Fachriansyah",
-    nrp: "5025241020",
+    nama: "Sersan Mirai Afrizal",
+    nrp: "5025241999",
     status: "UP",
     timestamp: new Date().toISOString(),
     uptime: `${Math.floor(process.uptime())}s`,
